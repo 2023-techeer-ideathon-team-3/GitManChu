dbc3ddf122ebffdaf3542e1ebc69be5873d0b1b8
ghKim <ghkim@whatap.io>
2023-05-27 05:28:04 +0900

    chore: init backend

--- backend/test.py
+++ backend/test.py
@@ -142,5 +142,42 @@ def add_post():
         else:
             return jsonify({'error': 'title 또는 content가 누락되었습니다.'}), 400
 
-if __name__ == '__main__':
-    app.run()
+from flask import Flask, redirect, url_for, session
+from authlib.integrations.flask_client import OAuth
+
+app = Flask(__name__)
+
+
+app.secret_key = 'myscretkey'
+#configuraciones de oauth
+oauth = OAuth(app)
+github = oauth.register(
+    name='github',
+    client_id='e4cf94d8a14d851411a7',
+    client_secret='81a00d25b89171de7303809cc53077b19070c6f3',
+    access_token_url='https://github.com/login/oauth/access_token',
+    access_token_params=None,
+    authorize_url='https://github.com/login/oauth/authorize',
+    authorize_params=None,
+    api_base_url='https://api.github.com/',
+    client_kwargs={'scope': 'user:email'},
+)
+@app.route('/login')
+def registro():
+   github = oauth.create_client('github')
+   redirect_uri = url_for('authorize', _external=True)
+   return github.authorize_redirect(redirect_uri)
+
+@app.route('/authorize')
+def authorize():
+   github = oauth.create_client('github')
+   token = github.authorize_access_token()
+   resp = github.get('user', token=token)
+   profile = resp.json()
+   # do something with the token and profile
+   print(profile, token)
+   return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
